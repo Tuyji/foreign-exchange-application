086fc825850e82388db0ed7cd46aa21128bc32ec
volkan.tuyji <volkan.tuyji@hititcs.com>
2019-11-15 15:35:17 +0300

    ConversionControllerTest

--- conversion-api/src/test/java/com/openpayd/conversion/ConversionControllerTest.java
+++ conversion-api/src/test/java/com/openpayd/conversion/ConversionControllerTest.java
@@ -1,22 +1,44 @@
 package com.openpayd.conversion;
 
-import org.junit.jupiter.api.extension.ExtendWith;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.core.convert.ConversionService;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
-@ExtendWith(SpringExtension.class)
-@WebMvcTest
+import com.openpayd.client.feign.FixerApiFeignClient;
+import com.openpayd.conversion.controller.ConversionController;
+
+@WebMvcTest(ConversionController.class)
 public class ConversionControllerTest {
 
 	@Autowired
-    MockMvc mockMvc;
+    MockMvc mvc;
 	
 	@MockBean
-	ConversionService service;
+	FixerApiFeignClient client;
 	
-	
+	@Test
+	public void testFindById() throws Exception{
+		mvc.perform(get("/conversions/id/1"))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+
+	@Test
+	public void testFindByDate() throws Exception{
+		mvc.perform(get("/conversions")
+				.param("date", LocalDate.now().toString())
+				.param("pageNo", "0")
+				.param("pageSize", "1"))
+		.andExpect(status().isOk())
+		.andDo(print());
+	}
+
 }
